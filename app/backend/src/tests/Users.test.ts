cad3ca04a76408b0bead153c7d06261842d008a8
Matheus Diógenes <mathdiogenes@gmail.com>
2023-03-02 12:06:52 -0300

    feat: R19

--- app/backend/src/tests/Users.test.ts
+++ app/backend/src/tests/Users.test.ts
@@ -4,33 +4,119 @@ import * as chai from 'chai';
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
+import UsersService from '../services/UsersService';
+import authMiddleware from '../middlewares/auth';
 import UserModel from '../database/models/UserModel';
 import jwt = require('jsonwebtoken');
-import UsersService from '../services/UsersService';
+
 import UsersController from '../controllers/UsersController';
+
+import bcrypt = require('bcryptjs');
+const usersService = new UsersService();
+const auth = new authMiddleware();
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('/users routes tests', function () {
+describe('/login routes tests', function () {
   afterEach(function() {
     sinon.restore();
   });
 
-  it('tests if it returns a token', async function () {
-    const outputMock = {
-      token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2Nzc3NjQ0OTh9.KpLVpXue-UbI6N233ZkqOnXVHsEg2BDcNH2JqRmwBwk'
-    };
+  it('tests if it returns the status code 200', async function () {
     const inputMock = {
       email: "admin@admin.com",
       password: "secret_admin",
     }
-    // sinon.stub(UsersService, 'validateEncryption').returns();
-    // sinon.stub(jwt, 'sign').returns(outputMock);
 
     const response = await chai.request(app).post('/login').send(inputMock);
     expect(response.status).to.be.eq(200);
-    // expect(response.body).to.deep.eq(outputMock);
+  });
+
+  it('tests if it returns the status code 400', async function () {
+    const inputMock = {
+      email: 'admin@admin.com',
+    }
+
+    const response = await chai.request(app).post('/login').send(inputMock);
+    expect(response.status).to.be.eq(400);
+  });
+
+  it('tests if invalid e-mail returns a UNAUTHORIZED error', async function () {
+    const inputMock = {
+      email: 'invalidemail@admin.com',
+      password: 'secret_admin',
+    }
+    const responseBody = {
+      message: 'Invalid email or password',
+    }
+    sinon.stub(usersService, 'validateLogin').resolves(null);
+
+    const response = await chai.request(app).post('/login').send(inputMock);
+    expect(response.status).to.be.eq(401);
+    expect(response.body).to.deep.eq(responseBody);
+  });
+
+  it('tests if invalid password returns a UNAUTHORIZED error', async function () {
+    const inputMock = {
+      email: 'admin@admin.com',
+      password: 'invalid-password',
+    }
+    const responseBody = {
+      message: 'Invalid email or password',
+    }
+    sinon.stub(bcrypt, 'compareSync').returns(false);
+
+    const response = await chai.request(app).post('/login').send(inputMock);
+    expect(response.status).to.be.eq(401);
+    expect(response.body).to.deep.eq(responseBody);
+  });
+
+  it('tests if a e-mail with invalid format returns a UNAUTHORIZED error', async function () {
+    const inputMock = {
+      email: 'admin@com',
+      password: 'secret_admin',
+    }
+    const responseBody = {
+      message: 'Invalid email or password',
+    }
+
+    const response = await chai.request(app).post('/login').send(inputMock);
+    expect(response.status).to.be.eq(401);
+    expect(response.body).to.deep.eq(responseBody);
+  });
+
+  it('tests if a password with less than 6 characters returns a UNAUTHORIZED error', async function () {
+    const inputMock = {
+      email: 'admin@admin.com',
+      password: '123',
+    }
+    const responseBody = {
+      message: 'Invalid email or password',
+    }
+
+    const response = await chai.request(app).post('/login').send(inputMock);
+    expect(response.status).to.be.eq(401);
+    expect(response.body).to.deep.eq(responseBody);
+  });
+
+  it('tests if the route /login/role returns status code 200', async function () {
+    sinon.stub(jwt, 'verify').callsFake(() => {
+      return Promise.resolve({success: 'Token is valid'});
+    });
+
+    const response = await chai.request(app)
+      .get('/login/role')
+      .set('authorization', 'token');
+    
+    expect(response.status).to.be.eq(200);
+  });
+
+  it('tests if it returns "Token not found" error', async function () {
+    const response = await chai.request(app).get('/login/role')
+    
+    expect(response.status).to.be.eq(401);
+    expect(response.body).to.deep.eq({ message: 'Token not found' });
   });
 });
